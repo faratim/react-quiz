3be94cb6918ebc2c8f9024d681484ef8fce6eff4
Tim <faratim@gmail.com>
2024-10-01 15:12:13 -0400

    starting to update using useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+function reducer(state, action) {
+  console.log(state, action)
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "SET":
+      return action.count;
+    default:
+      return state;
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0); 
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,17 +25,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "DEC" });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch( {type: "INC"});
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({type: 'SET', payload: Number(e.target.value)});
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +46,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
